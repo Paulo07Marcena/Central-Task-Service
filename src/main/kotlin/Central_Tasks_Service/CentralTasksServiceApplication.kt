7f4357d11b67d62dbdc5a9b68305b5a9a462358c
paulo07marcena <paulo.marcena@sptech.school>
2025-05-18 00:35:42 -0300

    feat: First commit, starting the application

---
+++ src/main/kotlin/Central_Tasks_Service/CentralTasksServiceApplication.kt
@@ -0,0 +1,11 @@
+package Central_Tasks_Service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CentralTasksServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CentralTasksServiceApplication>(*args)
+}
---
+++ src/test/kotlin/Central_Tasks_Service/CentralTasksServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package Central_Tasks_Service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CentralTasksServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
