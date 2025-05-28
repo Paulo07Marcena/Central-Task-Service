3729ab3c91dcf5381a122f6e0bf9c8a69e86e609
paulo07marcena <paulo.marcena@sptech.school>
2025-05-28 07:54:04 -0300

    feat: Change project structure (Kotlin to Java)

--- README.md
+++
@@ -1,1 +0,0 @@
-# Central-Task-Service
---
+++ src/main/java/com/paulo07reis/centraltask/CentralTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paulo07reis.centraltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralTaskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralTaskServiceApplication.class, args);
+	}
+
+}
